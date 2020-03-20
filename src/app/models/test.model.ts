cc43a2e0de33e290344a9395144e8c26505cf0fe
Antal Gyuri <antal.gyuri.88@gmail.com>
2020-03-20 23:30:33 +0100

    test model

---
+++ src/app/models/test.model.ts
@@ -0,0 +1,43 @@
+export class Test {
+    "@id": string = 'P24304';
+    "@title": string = 'Szövegdoboz';
+    "@null": any = null;
+    "@undefined": undefined;
+    "@required": boolean = true;
+    "@expectedNumber": number = 30;
+    "@unexpectedNumber": number = 100;
+    "@select1": string = '0';
+    "@color": string = 'RED';
+    config = {
+        "@select1": {
+            type: 'select',
+            options: [
+                {label: 'Option 1', value: 0},
+                {label: 'Option 2', value: 1},
+                {label: 'Option 3', value: 2},
+            ]
+        },
+        "@color": {
+            type: 'select',
+            options: [
+                {label: 'Zöld', value: 'GREEN'},
+                {label: 'Piros', value: 'RED'},
+                {label: 'Kék', value: 'BLUE'}
+            ]
+        },
+        "@required": {
+            type: 'checkbox',
+            options: null
+        },
+        "@expectedNumber": {
+            type: 'number',
+            options: [
+                10, // min
+                140, // max
+                5 // step
+            ]
+        }
+    };
+
+    constructor() {}
+}
